19c9ac76e381abe7b00a41423a72aa6f187f0fa9
Luc <luc.berger@octopusev.com>
2025-11-07 15:37:01 +0000

    feat: add clickable range finder functionality (ugly implementation)

--- src/components/DemoComponent.tsx
+++ src/components/DemoComponent.tsx
@@ -1,11 +1,58 @@
 import { useMapEvents } from 'react-leaflet/hooks'
+import axios from 'axios'
+import { Polygon } from 'react-leaflet/Polygon';
+import { useState } from 'react';
+
+function ringCoordsHashToArray(ring) {
+    return ring.map(function (latLng) {
+        return [latLng.lat, latLng.lng];
+    });
+}
+
+const purpleOptions = { color: 'purple' }
 
 export const DemoComponent = () => {
+
+    const [shapeCoords, setShapeCoords] = useState(null);
+
+    const triggerRequest = async (coords: any) => {
+        const response = await axios.post('https://api.traveltimeapp.com/v4/distance-map', {
+            departure_searches: [
+                {
+                    id: "isochrone-0",
+                    coords: {
+                        lat: coords.lat,
+                        lng: coords.lng
+                    },
+                    travel_distance: 10000,
+                    transportation: {
+                        type: "driving"
+                    },
+                    departure_time: "2025-11-07T09:00:00Z"
+                }
+            ]
+        }, {
+            headers: {
+                'X-Application-Id': import.meta.env.VITE_APP_ID,
+                'X-Api-Key': import.meta.env.VITE_APP_KEY,
+            }
+        });
+
+
+        const shapesCoords = response.data.results[0].shapes.map(function (polygon) {
+            const shell = ringCoordsHashToArray(polygon.shell);
+            const holes = polygon.holes.map(ringCoordsHashToArray);
+            return [shell].concat(holes);
+        });
+
+        setShapeCoords(shapesCoords);
+    }
+
     useMapEvents({
         click: (e) => {
-            console.log(e.latlng)
+            triggerRequest(e.latlng)
         }
     })
 
-    return <div>Demo Component</div>;
+    return shapeCoords ? <Polygon pathOptions={purpleOptions} positions={shapeCoords} /> : null;
 };
